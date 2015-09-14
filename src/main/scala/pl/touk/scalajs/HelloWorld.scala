151c406d85f78f8552258311536f85a6554c5c39
Maciek Próchniak <mpr@touk.pl>
2015-09-14 21:33:41 +0200

    helloWorld

---
+++ src/main/scala/pl/touk/scalajs/HelloWorld.scala
@@ -0,0 +1,17 @@
+package pl.touk.scalajs
+
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport
+class HelloWorld {
+
+  @JSExport
+  def run(): Unit = {
+
+    println("hello world")
+
+  }
+
+
+
+}
