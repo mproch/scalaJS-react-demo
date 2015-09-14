7eb52bdd1f21381e1c9786bfecd6dab68e978fd2
Maciek Próchniak <mpr@touk.pl>
2015-09-14 21:42:50 +0200

    helloDom

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ enablePlugins(ScalaJSPlugin)
 name := "scalaJDay"
 
 scalaVersion := "2.11.7"
+
+
+libraryDependencies ++= Seq(
+  "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+)
+
--- src/main/scala/pl/touk/scalajs/HelloWorld.scala
+++ src/main/scala/pl/touk/scalajs/HelloWorld.scala
@@ -2,6 +2,9 @@ package pl.touk.scalajs
 
 import scala.scalajs.js.annotation.JSExport
 
+import org.scalajs.dom
+import dom.document
+
 @JSExport
 class HelloWorld {
 
@@ -9,6 +12,13 @@ class HelloWorld {
   def run(): Unit = {
 
     println("hello world")
+
+    val inner = document.createElement("span")
+
+    inner.appendChild(document.createTextNode("hello World"))
+
+    document.body.appendChild(inner)
+    document.getElementById("content").appendChild(inner)
 
   }
 
