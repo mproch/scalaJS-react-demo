a353cafd944870923a0de93f57536822bccbb975
Maciek Próchniak <mpr@touk.pl>
2015-09-14 23:29:55 +0200

    locationWithComments

--- js/src/main/scala/pl/touk/scalajs/HelloWorld.scala
+++ js/src/main/scala/pl/touk/scalajs/HelloWorld.scala
@@ -2,14 +2,14 @@ package pl.touk.scalajs
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import org.scalajs.dom.ext.Ajax
+import org.scalajs.dom.html.Div
+import org.scalajs.dom.ext._
 
 import scala.concurrent.Future
-import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
-import scalatags.JsDom.attrs._
-import scalatags.JsDom.short._
-import scalatags.JsDom.tags._
+import scala.util.Failure
+import scalatags.JsDom.all._
+
 
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
@@ -18,36 +18,31 @@ import upickle.default._
 @JSExport
 class HelloWorld {
 
-  private def getData(data:String): Future[List[GeoLocation]] = {
-    val url =s"http://nominatim.openstreetmap.org/search?q=$data&format=json"
-    val result = Ajax.get(url).map(xhr => read[List[GeoLocation]](xhr.responseText))
-    result.onFailure {
-      case ex: Throwable => dom.window.alert(s"Failed with ${ex.getMessage}, $ex")
-    }
-    result
+  private def getData(data: String): Future[List[GeoLocationWithComments]] =
+    readAjax[List[GeoLocation]](s"http://nominatim.openstreetmap.org/search?q=$data&format=json")
+      .flatMap(list => Future.sequence(list.map(loc => getComments(loc.name).map(GeoLocationWithComments(loc, _)))))
+
+  private def getComments(oneLocation: String) =
+    readAjax[List[Comment]](s"http://localhost:8080/locationComment/$oneLocation")
+
+  private def readAjax[T: Reader](url: String) = Ajax.get(url)
+    .map(_.responseText).map(read[T]).andThen {
+    case Failure(ex) => dom.window.alert(s"Failed with ${ex.getMessage}, $ex")
   }
 
   @JSExport
   def run(): Unit = {
-
-    val list = ul().render
-
-    val innerContent = div(id := "content3", onclick := { () => {
-        println("hello?")
-        list.appendChild(li("JDay is gr8!").render)
-      }},
-      "hello world",
-      list
-    )
-
-    getData("Lviv").onSuccess {
-      case cont => cont.foreach(v => list.appendChild(li(s"${v.name}, ${v.latitude}, ${v.longitude}").render))
-    }
-
-    document.getElementById("content").appendChild(innerContent.render)
-
+    val inputBox = input(`type` := "text").render
+    val listDiv = div().render
+    document.body.appendChild(div(inputBox, button("Load", onclick := { () =>
+      getData(inputBox.value).foreach(fill(listDiv))
+    }), listDiv).render)
   }
 
+  private def fill(divel: Div)(locations: List[GeoLocationWithComments]) = {
+    divel.children.foreach(divel.removeChild)
+    divel.appendChild(ul(locations.map(loc => li(s"${loc.location.name}: ${loc.comments}"))).render)
 
+  }
 
 }
--- shared/src/main/scala/pl/touk/scalajs/GeoLocation.scala
+++ shared/src/main/scala/pl/touk/scalajs/GeoLocation.scala
@@ -6,3 +6,6 @@ case class GeoLocation(@key("display_name") name: String,
                        @key("lat") latitude: Double, @key("lon") longitude: Double)
 
 
+case class GeoLocationWithComments(location: GeoLocation, comments:List[Comment])
+
+
